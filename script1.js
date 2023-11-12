e5ce057917ffdae75ed929e9af36b83e5ed88810
the1shoot <krivou2808@gmail.com>
2023-11-12 16:04:09 +0100

    Site

---
+++ script1.js
@@ -0,0 +1,9 @@
+const zuGal = document.getElementById('zu')
+const uber = document.getElementById('ich')
+const fach = document.getElementById('fach')
+const galerie = document.getElementById('gal')
+
+uber.addEventListener('click', function(){window.location.href = "index.html"})
+fach.addEventListener('click', function(){window.location.href = "faehigkeiten.html"})
+zuGal.addEventListener('click', function func(){window.location.href = "Galerie.html"})
+galerie.addEventListener('click', function func(){window.location.href = "Galerie.html"})
