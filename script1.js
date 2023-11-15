66c1c53de81c07cd9db9642c728a02d54d87b11f
the1shoot <krivou2808@gmail.com>
2023-11-16 00:23:02 +0100

    wow

--- script1.js
+++ script1.js
@@ -3,7 +3,21 @@ const uber = document.getElementById('ich')
 const fach = document.getElementById('fach')
 const galerie = document.getElementById('gal')
 
-uber.addEventListener('click', function(){window.location.href = "index.html"})
+function color1(){document.body.classList.add("change1")}
+function color2(){document.body.classList.add("change2")} 
+function color3(){document.body.classList.add("change3")}          
+function scrolling(){
+    var scrollInterval = setInterval(function() {
+        window.scrollBy(0, 5);
+        if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+          clearInterval(scrollInterval);
+        }
+      }, 15);
+}
+
+uber.addEventListener('click', function(){window.location.href = "index.html"}); 
 fach.addEventListener('click', function(){window.location.href = "faehigkeiten.html"})
-zuGal.addEventListener('click', function func(){window.location.href = "Galerie.html"})
-galerie.addEventListener('click', function func(){window.location.href = "Galerie.html"})
+zuGal.addEventListener('click', function(){window.location.href = "Galerie.html"})
+galerie.addEventListener('click', function(){window.location.href = "Galerie.html"})
+
+
